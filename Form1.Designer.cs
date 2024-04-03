d2001390c32b24579c1e7d06e7f6f26a10c98fc0
SyedHasanHussainShah <165957231+SyedHasanHussainShah@users.noreply.github.com>
2024-04-04 01:52:22 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace oppp_ass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gunaGroupBox1 = new Guna.UI.WinForms.GunaGroupBox();
+            this.gunaGroupBox2 = new Guna.UI.WinForms.GunaGroupBox();
+            this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel2 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel3 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel4 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel5 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel6 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel7 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel8 = new Guna.UI.WinForms.GunaLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.gunaGroupBox1.SuspendLayout();
+            this.gunaGroupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Soda ",
+            "Tea ",
+            "Coffee",
+            "Minearl Water",
+            "Juice",
+            "Milk"});
+            this.comboBox1.Location = new System.Drawing.Point(11, 101);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 33);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Items Block 1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Buffalo wings",
+            "Buffalo Fingers",
+            "Potato skins",
+            "Nachos",
+            "Mushroom Chaps",
+            "Shrimp Coocktail",
+            "Cips and salsa"});
+            this.comboBox2.Location = new System.Drawing.Point(225, 101);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(173, 33);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.Text = "Items Block 2";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Seafood Alfredo",
+            "Chicken Alferdo",
+            "Chicken Pateeta",
+            "Turkey Club",
+            "Losber Pie",
+            "Prime rub",
+            "Scrimp sacert",
+            "Turkey dinner",
+            "Stuffed Chicken"});
+            this.comboBox3.Location = new System.Drawing.Point(441, 101);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(171, 33);
+            this.comboBox3.TabIndex = 2;
+            this.comboBox3.Text = "Items Block 3";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Apple Pie",
+            "Sundae",
+            "Carrot cake",
+            "Mud pie",
+            "Apple crisp"});
+            this.comboBox4.Location = new System.Drawing.Point(640, 101);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(179, 33);
+            this.comboBox4.TabIndex = 3;
+            this.comboBox4.Text = "Items Block 4";
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 170);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(398, 306);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(246, 145);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 35);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Amount=$";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 13F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(41, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 33);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Total Bill            $";
+            // 
+            // gunaGroupBox1
+            // 
+            this.gunaGroupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaGroupBox1.BaseColor = System.Drawing.Color.White;
+            this.gunaGroupBox1.BorderColor = System.Drawing.Color.Gainsboro;
+            this.gunaGroupBox1.Controls.Add(this.gunaLabel8);
+            this.gunaGroupBox1.Controls.Add(this.gunaLabel7);
+            this.gunaGroupBox1.Controls.Add(this.gunaLabel6);
+            this.gunaGroupBox1.Controls.Add(this.gunaLabel5);
+            this.gunaGroupBox1.Controls.Add(this.gunaLabel4);
+            this.gunaGroupBox1.Controls.Add(this.gunaLabel3);
+            this.gunaGroupBox1.Controls.Add(this.gunaLabel2);
+            this.gunaGroupBox1.Controls.Add(this.gunaLabel1);
+            this.gunaGroupBox1.Controls.Add(this.comboBox1);
+            this.gunaGroupBox1.Controls.Add(this.comboBox2);
+            this.gunaGroupBox1.Controls.Add(this.comboBox3);
+            this.gunaGroupBox1.Controls.Add(this.comboBox4);
+            this.gunaGroupBox1.LineColor = System.Drawing.Color.DarkMagenta;
+            this.gunaGroupBox1.Location = new System.Drawing.Point(1, -1);
+            this.gunaGroupBox1.Name = "gunaGroupBox1";
+            this.gunaGroupBox1.Size = new System.Drawing.Size(819, 170);
+            this.gunaGroupBox1.TabIndex = 7;
+            this.gunaGroupBox1.TextLocation = new System.Drawing.Point(10, 8);
+            // 
+            // gunaGroupBox2
+            // 
+            this.gunaGroupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.gunaGroupBox2.BaseColor = System.Drawing.Color.White;
+            this.gunaGroupBox2.BorderColor = System.Drawing.Color.DarkViolet;
+            this.gunaGroupBox2.Controls.Add(this.textBox1);
+            this.gunaGroupBox2.Controls.Add(this.label1);
+            this.gunaGroupBox2.LineColor = System.Drawing.Color.DarkMagenta;
+            this.gunaGroupBox2.Location = new System.Drawing.Point(395, 170);
+            this.gunaGroupBox2.Name = "gunaGroupBox2";
+            this.gunaGroupBox2.Size = new System.Drawing.Size(425, 306);
+            this.gunaGroupBox2.TabIndex = 8;
+            this.gunaGroupBox2.TextLocation = new System.Drawing.Point(10, 8);
+            // 
+            // gunaLabel1
+            // 
+            this.gunaLabel1.AutoSize = true;
+            this.gunaLabel1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaLabel1.Location = new System.Drawing.Point(56, 54);
+            this.gunaLabel1.Name = "gunaLabel1";
+            this.gunaLabel1.Size = new System.Drawing.Size(84, 25);
+            this.gunaLabel1.TabIndex = 4;
+            this.gunaLabel1.Text = "Beverage";
+            // 
+            // gunaLabel2
+            // 
+            this.gunaLabel2.AutoSize = true;
+            this.gunaLabel2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel2.ForeColor = System.Drawing.Color.White;
+            this.gunaLabel2.Location = new System.Drawing.Point(47, 0);
+            this.gunaLabel2.Name = "gunaLabel2";
+            this.gunaLabel2.Size = new System.Drawing.Size(104, 25);
+            this.gunaLabel2.TabIndex = 5;
+            this.gunaLabel2.Text = "Catagery 1";
+            // 
+            // gunaLabel3
+            // 
+            this.gunaLabel3.AutoSize = true;
+            this.gunaLabel3.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaLabel3.Location = new System.Drawing.Point(261, 54);
+            this.gunaLabel3.Name = "gunaLabel3";
+            this.gunaLabel3.Size = new System.Drawing.Size(93, 25);
+            this.gunaLabel3.TabIndex = 6;
+            this.gunaLabel3.Text = " Appetizer";
+            // 
+            // gunaLabel4
+            // 
+            this.gunaLabel4.AutoSize = true;
+            this.gunaLabel4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel4.ForeColor = System.Drawing.Color.White;
+            this.gunaLabel4.Location = new System.Drawing.Point(261, 0);
+            this.gunaLabel4.Name = "gunaLabel4";
+            this.gunaLabel4.Size = new System.Drawing.Size(104, 25);
+            this.gunaLabel4.TabIndex = 7;
+            this.gunaLabel4.Text = "Catagery 2";
+            // 
+            // gunaLabel5
+            // 
+            this.gunaLabel5.AutoSize = true;
+            this.gunaLabel5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel5.ForeColor = System.Drawing.Color.White;
+            this.gunaLabel5.Location = new System.Drawing.Point(459, 0);
+            this.gunaLabel5.Name = "gunaLabel5";
+            this.gunaLabel5.Size = new System.Drawing.Size(104, 25);
+            this.gunaLabel5.TabIndex = 8;
+            this.gunaLabel5.Text = "Catagery 3";
+            // 
+            // gunaLabel6
+            // 
+            this.gunaLabel6.AutoSize = true;
+            this.gunaLabel6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel6.ForeColor = System.Drawing.Color.White;
+            this.gunaLabel6.Location = new System.Drawing.Point(658, 0);
+            this.gunaLabel6.Name = "gunaLabel6";
+            this.gunaLabel6.Size = new System.Drawing.Size(104, 25);
+            this.gunaLabel6.TabIndex = 9;
+            this.gunaLabel6.Text = "Catagery 4";
+            // 
+            // gunaLabel7
+            // 
+            this.gunaLabel7.AutoSize = true;
+            this.gunaLabel7.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaLabel7.Location = new System.Drawing.Point(459, 54);
+            this.gunaLabel7.Name = "gunaLabel7";
+            this.gunaLabel7.Size = new System.Drawing.Size(111, 25);
+            this.gunaLabel7.TabIndex = 10;
+            this.gunaLabel7.Text = "Main Course";
+            // 
+            // gunaLabel8
+            // 
+            this.gunaLabel8.AutoSize = true;
+            this.gunaLabel8.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaLabel8.Location = new System.Drawing.Point(673, 54);
+            this.gunaLabel8.Name = "gunaLabel8";
+            this.gunaLabel8.Size = new System.Drawing.Size(71, 25);
+            this.gunaLabel8.TabIndex = 11;
+            this.gunaLabel8.Text = "Dessert";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::oppp_ass.Properties.Resources.OIP;
+            this.ClientSize = new System.Drawing.Size(820, 479);
+            this.Controls.Add(this.gunaGroupBox2);
+            this.Controls.Add(this.gunaGroupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.gunaGroupBox1.ResumeLayout(false);
+            this.gunaGroupBox1.PerformLayout();
+            this.gunaGroupBox2.ResumeLayout(false);
+            this.gunaGroupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI.WinForms.GunaGroupBox gunaGroupBox1;
+        private Guna.UI.WinForms.GunaGroupBox gunaGroupBox2;
+        private Guna.UI.WinForms.GunaLabel gunaLabel8;
+        private Guna.UI.WinForms.GunaLabel gunaLabel7;
+        private Guna.UI.WinForms.GunaLabel gunaLabel6;
+        private Guna.UI.WinForms.GunaLabel gunaLabel5;
+        private Guna.UI.WinForms.GunaLabel gunaLabel4;
+        private Guna.UI.WinForms.GunaLabel gunaLabel3;
+        private Guna.UI.WinForms.GunaLabel gunaLabel2;
+        private Guna.UI.WinForms.GunaLabel gunaLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oppp_ass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataTable dt = new DataTable();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Price");
+            dataGridView1.DataSource = dt;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Soda ")
+            {
+                dt.Rows.Add("Soda", "1.95");
+            }
+            else if (comboBox1.Text == "Tea ")
+            {
+                dt.Rows.Add("Tea ", "1.50");
+            }
+            else if (comboBox1.Text == "Coffee")
+            {
+                dt.Rows.Add("Coffee", "1.25");
+            }
+            else if (comboBox1.Text == "Minearl Water")
+            {
+                dt.Rows.Add("Minearl Water", "2.95");
+            }
+            else if (comboBox1.Text == "Juice")
+            {
+                dt.Rows.Add("Juice", "2.50");
+            }
+            else if (comboBox1.Text == "Milk")
+            {
+                dt.Rows.Add("Milk", "1.50");
+            }
+            getPrice();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (comboBox2.Text == "Buffalo wings")
+            {
+                dt.Rows.Add("Buffalo wings", "5.95");
+            }
+            else if (comboBox2.Text == "Buffalo Fingers")
+            {
+                dt.Rows.Add("Buffalo Fingers", "5.95");
+            }
+            else if (comboBox2.Text == "Potato skins")
+            {
+                dt.Rows.Add("Potato skins", "8.95");
+            }
+            else if (comboBox2.Text == "Nachos")
+            {
+                dt.Rows.Add("Nachos", "8.95");
+            }
+            else if (comboBox2.Text == "Mushroom Chaps")
+            {
+                dt.Rows.Add("Mushroom Chaps", "10.95");
+            }
+            else if (comboBox2.Text == "Shrimp Coocktail")
+            {
+                dt.Rows.Add("Shrimp Coocktail", "12.95");
+            }
+            else if (comboBox2.Text == "Shrimp Coocktail")
+            {
+                dt.Rows.Add("Shrimp Coocktail", "12.95");
+            }
+            else if (comboBox2.Text == "Cips and salsa")
+            {
+                dt.Rows.Add("Cips and salsa", "6.95");
+            }
+            getPrice();
+        }
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox3.Text == "Seafood Alfredo")
+            {
+                dt.Rows.Add("Seafood Alfredo", "15.95");
+            }
+            else if (comboBox3.Text == "Chicken Alferdo")
+            {
+                dt.Rows.Add("Chicken Alferdo", "13.95");
+            }
+            else if (comboBox3.Text == "Chicken Pateeta")
+            {
+                dt.Rows.Add("Chicken Pateeta", "13.95");
+            }
+            else if (comboBox3.Text == "Turkey Club")
+            {
+                dt.Rows.Add("Turkey Club", "11.95");
+            }
+            else if (comboBox3.Text == "Losber Pie")
+            {
+                dt.Rows.Add("Losber Pie", "19.95");
+            }
+            else if (comboBox3.Text == "Prime rub")
+            {
+                dt.Rows.Add("Prime rub", "20.95");
+            }
+            else if (comboBox3.Text == "Scrimp sacert")
+            {
+                dt.Rows.Add("Scrimp sacert", "18.95");
+            }
+            else if (comboBox3.Text == "Turkey dinner")
+            {
+                dt.Rows.Add("Turkey dinner", "13.95");
+            }
+            else if (comboBox3.Text == "Stuffed Chicken")
+            {
+                dt.Rows.Add("Stuffed Chicken", "14.95");
+            }
+            getPrice();
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox4.Text == "Apple Pie")
+            {
+                dt.Rows.Add("Apple Pie", "5.95");
+            }
+            else if (comboBox4.Text == "Sundae")
+            {
+                dt.Rows.Add("Sundae", "3.95");
+            }
+            else if (comboBox4.Text == "Carrot cake")
+            {
+                dt.Rows.Add("Carrot cake", "5.95");
+            }
+            else if (comboBox4.Text == "Mud pie")
+            {
+                dt.Rows.Add("Mud pie", "4.95");
+            }
+            else if (comboBox4.Text == "Apple crisp")
+            {
+                dt.Rows.Add("Apple crisp", "5.95");
+            }
+            getPrice();
+        }
+        public void getPrice()
+        {
+            double total = 0;
+            foreach (DataRow item in dt.Rows)
+            {
+                total += Convert.ToDouble(item["Price"]);
+            }
+            textBox1.Text = total.ToString();
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
